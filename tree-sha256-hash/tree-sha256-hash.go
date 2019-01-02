f82ca3ff463e7149644cda76b9226a849a7f3e24
ivan386 <ivan386@users.noreply.github.com>
2019-01-02 18:10:28 +0300

    NewHashList

--- tree-sha256-hash/tree-sha256-hash.go
+++ tree-sha256-hash/tree-sha256-hash.go
@@ -1,8 +1,13 @@
 package tree_sha256_hash
 
-import "crypto/sha256"
-import "github.com/ivan386/go-hash-tree"
+import sha256 "crypto/sha256"
+import hash_tree "github.com/ivan386/go-hash-tree"
+import hash_list "github.com/ivan386/go-hash-list"
 
 func New() *hash_tree.Tree {
 	return hash_tree.New(sha256.New(), 1024, []byte{0x00}, []byte{0x01}, true)
+}
+
+func NewHashList(level uint) *hash_list.List {
+	return hash_list.New(hash_tree.New(sha256.New(), 1024, []byte{0x00}, []byte{0x01}, true), 1024 << level)
 }
--- tree-tiger-hash/tree-tiger-hash.go
+++ tree-tiger-hash/tree-tiger-hash.go
@@ -1,8 +1,13 @@
 package tree_tiger_hash
 
-import "github.com/cxmcc/tiger"
-import "github.com/ivan386/go-hash-tree"
+import tiger "github.com/cxmcc/tiger"
+import hash_tree "github.com/ivan386/go-hash-tree"
+import hash_list "github.com/ivan386/go-hash-list"
 
 func New() *hash_tree.Tree {
 	return hash_tree.New(tiger.New(), 1024, []byte{0x00}, []byte{0x01}, true)
+}
+
+func NewHashList(level uint) *hash_list.List {
+	return hash_list.New(hash_tree.New(tiger.New(), 1024, []byte{0x00}, []byte{0x01}, true), 1024 << level)
 }
